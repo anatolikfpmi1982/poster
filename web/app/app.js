9d04bdc0cb6c44932170ce62031955dd0d3984d4
anatoly.bogdanov <anatolikfpmi@tut.by>
2017-11-23 20:20:49 +0300

    slider

--- web/app/app.js
+++ web/app/app.js
@@ -9,21 +9,7 @@ define(function (require, exports, module) {
     $(document).ready(function () {
         init();
 
-        $('#myCarousel2').cycle({
-            fx: 'fadeout',
-            speed: 300,
-            timeout: 1000,
-            autoHeight: 'calc',
-            slides: "img",
-            nextEvent: 'cycle-after',
-            pager: '.carousel-indicators',
-            pagerActiveClass: 'active',
-            next: '#carousel-control-right',
-            prev: '#carousel-control-left'
-        }).on('cycle-after', function (e, optionHash, outgoingSlideEl, incomingSlideEl, forwardFlag) {
-            $('.carousel-inner div').removeClass('active');
-            $('#data-slide-' + $(incomingSlideEl).data('slide-num')).addClass('active');
-        });
+
     });
 
     function onBefore(e, opts, outgoing, incoming, forward) {
