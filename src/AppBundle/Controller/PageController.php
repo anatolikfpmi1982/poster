91caddb8406b9a5a9a0cff5c2400a67936dd9bb0
dmitry <bubom@yandex.ru>
2017-11-27 20:18:55 +0300

    pages

---
+++ src/AppBundle/Controller/PageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Class PageController
+ */
+class PageController extends Controller
+{
+    /**
+     * @Route("/page/{slug}", name="page")
+     */
+    public function showAction($slug)
+    {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $page = $em->getRepository('AppBundle:Page')->findOneBy(['slug' => $slug, 'isActive' => true]);
+
+        // parameters to template
+        return $this->render('AppBundle:Page:index.html.twig', ['page' => $page]);
+    }
+
+}
