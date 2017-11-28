a1045cda8f68e3f9056425e29c16f6727d8e4aff
dmitry <bubom@yandex.ru>
2017-11-29 00:42:39 +0300

    settings form (only view)

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -1,14 +1,16 @@
 <?php
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sonata\CoreBundle\Form\Type\BooleanType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sonata\AdminBundle\Controller\CoreController;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 /**
  * Class AdminController
@@ -17,13 +19,22 @@ class AdminController extends CoreController
 {
     public function settingsAction(Request $request)
     {
-        // здесь размещается код вашей страницы
-        // ...
-
         $form = $this->createFormBuilder()
-            ->add('task', TextType::class)
-            ->add('dueDate', DateType::class)
-            ->add('save', SubmitType::class, array('label' => 'Create Task'))
+            ->add('phone', TextType::class, ['label' => 'Телефон'])
+            ->add('email', EmailType::class, ['label' => 'Email'])
+            ->add('contacts', TextareaType::class, ['label' => 'Контактные данные'])
+            ->add('title', TextType::class, ['label' => 'Заголовок'])
+            ->add('logo', FileType::class, ['label' => 'Логотип'])
+            ->add('under_slider_text', TextAreaType::class, ['label' => 'Текст под слайдером'])
+            ->add('favicon', FileType::class, ['label' => 'Favicon'])
+            ->add('metrics_yandex', TextAreaType::class, ['label' => 'Метрики Яндекс'])
+            ->add('metrics_google', TextAreaType::class, ['label' => 'Метрики Google'])
+            ->add('seo_keywords', TextAreaType::class, ['label' => 'SEO - Keywords'])
+            ->add('seo_description', TextAreaType::class, ['label' => 'SEO - Description'])
+            ->add('seo_title', TextType::class, ['label' => 'SEO - Title'])
+            ->add('enable_call_back', BooleanType::class, ['label' => 'Использовать функцию "Заказать звонок"'])
+            ->add('enable_sms', BooleanType::class, ['label' => 'Отправлять SMS после заказа звонка'])
+            ->add('save', SubmitType::class, array('label' => 'Сохранить'))
             ->getForm();
 
         return $this->render('AppBundle:Admin:settings.html.twig', array(
