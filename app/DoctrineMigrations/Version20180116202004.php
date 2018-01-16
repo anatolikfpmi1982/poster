9585014437799527e4496c40eea39b71aaa37fab
dmitry <bubom@yandex.ru>
2018-01-17 01:17:59 +0300

    order

---
+++ app/DoctrineMigrations/Version20180116202004.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180116202004 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE orders ADD is_done TINYINT(1) NOT NULL, DROP address2, DROP fax');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE orders ADD address2 VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci, ADD fax VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci, DROP is_done');
+    }
+}
