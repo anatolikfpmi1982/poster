c7e3de895d4e746fdefc57daca3b64f9e85ad6dc
dmitry <bubom@yandex.ru>
2018-01-20 00:12:50 +0300

    randomize

--- src/AppBundle/Controller/CategoriesCRUDController.php
+++ src/AppBundle/Controller/CategoriesCRUDController.php
@@ -3,65 +3,16 @@ namespace AppBundle\Controller;
 
 use Exception;
 use Sonata\AdminBundle\Controller\CRUDController as BaseController;
-use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Response;
 
-class CRUDController extends BaseController
+class CategoriesCRUDController extends BaseController
 {
     /**
-     * @param ProxyQueryInterface $selectedModelQuery
-     * @param Request             $request
-     *
-     * @return RedirectResponse
-     */
-    public function batchActionMerge(ProxyQueryInterface $selectedModelQuery, Request $request = null)
-    {
-        $this->admin->checkAccess('edit');
-        $this->admin->checkAccess('delete');
-
-        $modelManager = $this->admin->getModelManager();
-
-        $target = $modelManager->find($this->admin->getClass(), $request->get('targetId'));
-
-        if ($target === null){
-            $this->addFlash('sonata_flash_info', 'flash_batch_merge_no_target');
-
-            return new RedirectResponse(
-                $this->admin->generateUrl('list', array('filter' => $this->admin->getFilterParameters()))
-            );
-        }
-
-        $selectedModels = $selectedModelQuery->execute();
-
-        // do the merge work here
-
-        try {
-            foreach ($selectedModels as $selectedModel) {
-                $modelManager->delete($selectedModel);
-            }
-
-            $modelManager->update($selectedModel);
-        } catch (Exception $e) {
-            $this->addFlash('sonata_flash_error', 'flash_batch_merge_error');
-
-            return new RedirectResponse(
-                $this->admin->generateUrl('list', array('filter' => $this->admin->getFilterParameters()))
-            );
-        }
-
-        $this->addFlash('sonata_flash_success', 'flash_batch_merge_success');
-
-        return new RedirectResponse(
-            $this->admin->generateUrl('list', array('filter' => $this->admin->getFilterParameters()))
-        );
-    }
-
-    /**
-     * Delete action.
+     * Generate action.
      *
      * @param int|string|null $id
      *
@@ -129,10 +80,8 @@ class CRUDController extends BaseController
 //            return $this->redirectTo($object);
 //        }
 
-        $this->addFlash('sonata_flash_success', 'Cloned successfully');
+        $this->addFlash('sonata_flash_success', 'Рандомизация выполнена успешно!');
 
         return new RedirectResponse($this->admin->generateUrl('list', ['filter' => $this->admin->getFilterParameters()]));
     }
-
-    // ...
 }
