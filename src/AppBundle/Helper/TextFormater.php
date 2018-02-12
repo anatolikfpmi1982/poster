1bad55d11f7cc185a92cd6c4b2fb6fde2a0d48ce
dmitry <bubom@yandex.ru>
2018-02-12 21:15:55 +0300

    21#8

--- src/AppBundle/Helper/TextFormater.php
+++ src/AppBundle/Helper/TextFormater.php
@@ -14,9 +14,11 @@ class TextFormater {
     public static function formatMoreText($text) {
         $strArray =  explode('<!--more-->', $text);
         if(count($strArray) >= 2) {
-            $text = $strArray[0] . '</p>'
-                . '<a id="more_text" onclick="$(\'#more_more\').show();$(\'#more_text\').remove(); "></a>'
-                . '<div id="more_more" style="display:none;"><p>' . $strArray[1] . '</div>';
+            $text = '<div>' . (mb_strpos($strArray[0], '<p>', -3, 'UTF-8') !== null  ? mb_substr($strArray[0], 0, -3, 'UTF-8') : $strArray[0])
+                . '</div>' . '<span id="more_text" onclick="$(\'#more_more\').show();$(\'#more_text\').remove(); ">...</span>'
+                . '<div id="more_more" style="display:none;">'
+                . (mb_strpos($strArray[1], '</p>', 0, 'UTF-8') === 0  ? mb_substr($strArray[1], 4, mb_strlen($strArray[1], 'UTF-8'), 'UTF-8') : $strArray[1])
+                . '</div>';
         }
 
         return $text;
