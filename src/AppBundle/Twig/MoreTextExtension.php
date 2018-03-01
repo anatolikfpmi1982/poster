8a8dd48cd8048b17515093ef5743e2327860901d
dmitry <bubom@yandex.ru>
2018-03-01 09:57:04 +0300

    new more fix

--- src/AppBundle/Twig/MoreTextExtension.php
+++ src/AppBundle/Twig/MoreTextExtension.php
@@ -15,6 +15,9 @@ class MoreTextExtension extends AbstractExtension {
 
     public function moreTextFilter($text)
     {
+        $text = str_replace('<!--more-open-->', "<span class='more_open' onclick='$(this).next().show();$(this).next().next().css(\"display\", \"inline-block\");$(this).hide();return false;'></span><div class='more_block'>", $text);
+        $text = str_replace('<!--more-close-->', "</div><span class='more_close' onclick='$(this).prev().hide();$(this).prev().prev().css(\"display\", \"inline-block\");$(this).hide();return false;'></span>", $text);
+
         $strArray = explode( '<!--more-->', $text );
         if ( count( $strArray ) >= 2 ) {
             $text = '<div class="row">
